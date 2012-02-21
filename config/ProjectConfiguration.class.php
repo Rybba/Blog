2b8314c012d41230f9937f53d2ddf8804c507655
Krzysztof Rybski <Krzysiek@KrzysiekDesktop.(none)>
2012-02-21 19:20:51 +0100

    initial project version

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'G://Instalacje//symfony-1.4.15//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
